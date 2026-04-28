4142037c19eccf8676ee9a3d01234e9500fce1be
FaisalGul786 <faisalgull3.o@gmail.com>
2026-04-28 17:36:04 +0500

    resolve server.js file

--- src/server.js
+++ src/server.js
@@ -6,5 +6,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${POST} `);
+  console.log(`Server is running on port ${PORT} `);
 });
